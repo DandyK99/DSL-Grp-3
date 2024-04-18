790473f901a81541ef26f92eb3d34544dd43e553
swhx14 <91124750+swhx14@users.noreply.github.com>
2024-04-18 13:19:10 +0800

    Update prng.v

--- DSL 2D Project/prng.v
+++ DSL 2D Project/prng.v
@@ -9,27 +9,6 @@ module prng(
     integer i;
     reg reverse_num;
 
-//    always @* begin
-        // Your combinational logic for data_next
-        // Example logic (replace this with your actual logic)
-//        data_next[15] = data[15] ^ data[1];
-//        data_next[14] = data[14] ^ data[0];
-//        data_next[13] = data[13] ^ data_next[15];
-//        data_next[12] = data[12] ^ data_next[14];
-//        data_next[11] = data[11] ^ data_next[13];
-//        data_next[10] = data[10] ^ data_next[12];
-//        data_next[9]  = data[9]  ^ data_next[11];
-//        data_next[8]  = data[8]  ^ data_next[10];
-//        data_next[7]  = data[7]  ^ data_next[9];
-//        data_next[6]  = data[6]  ^ data_next[8];
-//        data_next[5]  = data[5]  ^ data_next[7];
-//        data_next[4]  = data[4]  ^ data_next[6];
-//        data_next[3]  = data[3]  ^ data_next[5];
-//        data_next[2]  = data[2]  ^ data_next[4];
-//        data_next[1]  = data[1]  ^ data_next[3];
-//        data_next[0]  = data[0]  ^ data_next[2];
-//    end
-
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             data_next <= seed;
